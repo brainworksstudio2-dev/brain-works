e4b1e9ea8f6d82ae2c2b69879c1a2061a5e4f294
Brain works Studio <brainworksstudio2@gmail.com>
2025-08-20 07:35:44 +0000

    put importtat thimgs in the env file ..the keys and firebase stuffs

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth, Auth } from 'firebase/auth';
 
 const firebaseConfig = {
-  projectId: 'brain-works-egf0a',
-  appId: '1:99514749199:web:9ffb83d923ec0ac1c56e7a',
-  storageBucket: 'brain-works-egf0a.firebasestorage.app',
-  apiKey: 'AIzaSyBZwtsuYr4RsYduEZgrEd_CmH5yOCevVCE',
-  authDomain: 'brain-works-egf0a.firebaseapp.com',
-  messagingSenderId: '99514749199',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
